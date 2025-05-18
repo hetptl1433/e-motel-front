aa088adb1e9a64b90138ba22c71f35f25b9c616c
hetptl1433 <hetptl1433@gmail.com>
2025-05-18 16:58:48 -0400

    add route for HouseKeeping component in App

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<HouseKeeping />} />
           <Route
             path="/housekeeping"
             element={

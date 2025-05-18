62523b52018967a47b8fbf701d0a954e958132e2
hetptl1433 <hetptl1433@gmail.com>
2025-05-18 17:35:48 -0400

    fix routing in App component to display Login on root path

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<HouseKeeping />} />
+          <Route path="/" element={<Login />} />
           <Route
             path="/housekeeping"
             element={
